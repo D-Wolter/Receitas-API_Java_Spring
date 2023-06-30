9be7bc1777a2de19d812ca3137691db6713dd421
D-Wolter <wolter.daniel@gmail.com>
2023-06-30 10:47:36 -0300

    feat: Atualizando as informações no banco de dados com endpoint PUT

--- src/main/java/com/example/receitaapi/model/ReceitaModel.java
+++ src/main/java/com/example/receitaapi/model/ReceitaModel.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,14 +30,31 @@ public class ReceitaModel {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @JsonInclude(JsonInclude.Include.NON_NULL)
   private Long id;
-  private String nome;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
-  private Date dataInclusao;
+
+  @Column(name = "name", length = 100, nullable = false)
   @JsonInclude(JsonInclude.Include.NON_NULL)
+  private String nome;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @Temporal(TemporalType.DATE)
+  private Date dataInclusao;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @Column(name = "autor", length = 100, nullable = false)
   private String autor;
+
   @Transient
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private List<Ingredinte> ingredientes;
+  
+  @Lob
+  @Column(name = "modo_de_preparo", nullable = false)
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String modoDePreparo;
+
+  @Column(name = "descricao", length = 250, nullable = false)
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String descricao;
 
 

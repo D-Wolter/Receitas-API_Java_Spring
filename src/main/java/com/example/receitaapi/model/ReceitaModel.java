a1b44e68107bec03525270cc7735dfe4f50ca803
D-Wolter <wolter.daniel@gmail.com>
2023-06-30 09:48:37 -0300

    feat: Gravando e recuperando informações do banco de dados

--- src/main/java/com/example/receitaapi/model/ReceitaModel.java
+++ src/main/java/com/example/receitaapi/model/ReceitaModel.java
@@ -1,11 +1,15 @@
 package com.example.receitaapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,21 +19,28 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class ReceitaModel {
- private Long id;
- private String nome;
- private Date dataInclusao;
- @JsonInclude(JsonInclude.Include.NON_NULL)
- private String autor;
- private List<Ingredinte> ingredientes;
- private String modoDePreparo;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private Long id;
+  private String nome;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+  private Date dataInclusao;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private String autor;
+  @Transient
+  private List<Ingredinte> ingredientes;
+  private String modoDePreparo;
+  private String descricao;
 
 
-
-  public ReceitaModel(long id, Date dataInclusao, String nomeReceita) {
+  public ReceitaModel(long id, String descricao, String nomeReceita) {
     this.id = id;
-    this.dataInclusao = dataInclusao;
+    this.descricao = descricao;
     this.nome = nomeReceita;
   }
-
 }
+
